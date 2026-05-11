0f13b47c2cc4abe0ef5526b0afc01ca193e169a3
FraDev85 <comunalefrancesco@gmail.com>
2026-05-11 12:05:07 +0200

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
-  base: "/TyperFast/TypeFaster",
+  base: "/typerfast/",
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
 });
